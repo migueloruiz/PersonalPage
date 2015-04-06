07687cfcf6db9cc2c61022aa84f9251415d8def3
Miguelo Ruiz <red2000ace@gmail.com>
2015-04-06 00:52:00 -0500

    change

--- _include/js/main.js
+++ _include/js/main.js
@@ -78,10 +78,10 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : 'https://raw.githubusercontent.com/miguueelo/PersonalPage/gh-pages/_include/img/slider-images/F1.JPG', title : '<img src="https://raw.githubusercontent.com/miguueelo/PersonalPage/gh-pages/_include/img/logo_slider.png" alt="">', thumb : '', url : ''},
-											{image : 'https://raw.githubusercontent.com/miguueelo/PersonalPage/gh-pages/_include/img/slider-images/F2.JPG', title : '<img src="https://raw.githubusercontent.com/miguueelo/PersonalPage/gh-pages/_include/img/logo_slider.png" alt="">', thumb : '', url : ''},
-											{image : 'https://raw.githubusercontent.com/miguueelo/PersonalPage/gh-pages/_include/img/slider-images/F1.JPG', title : '<img src="https://raw.githubusercontent.com/miguueelo/PersonalPage/gh-pages/_include/img/logo_slider.png" alt="">', thumb : '', url : ''},
-											{image : 'https://raw.githubusercontent.com/miguueelo/PersonalPage/gh-pages/_include/img/slider-images/F2.JPG', title : '<img src="https://raw.githubusercontent.com/miguueelo/PersonalPage/gh-pages/_include/img/logo_slider.png" alt="">', thumb : '', url : ''}  
+											{image : '.._include/img/slider-images/F1.JPG', title : '<img src=".._include/img/logo_slider.png" alt="">', thumb : '', url : ''},
+											{image : '.._include/img/slider-images/F2.JPG', title : '<img src=".._include/img/logo_slider.png" alt="">', thumb : '', url : ''},
+											{image : '.._include/img/slider-images/F1.JPG', title : '<img src=".._include/img/logo_slider.png" alt="">', thumb : '', url : ''},
+											{image : '.._include/img/slider-images/F2.JPG', title : '<img src=".._include/img/logo_slider.png" alt="">', thumb : '', url : ''}  
 									],
 									
 		// Theme Options			   
@@ -406,7 +406,7 @@ $(document).ready(function(){
 	Modernizr.load([
 	{
 		test: Modernizr.placeholder,
-		nope: 'https://raw.githubusercontent.com/miguueelo/PersonalPage/gh-pages/_include/js/placeholder.js', 
+		nope: '.._include/js/placeholder.js', 
 		complete : function() {
 				if (!Modernizr.placeholder) {
 						Placeholders.init({
